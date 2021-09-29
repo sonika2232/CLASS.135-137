ffc7b226878d5b41f4e94070918d87c652cfd2e0
sonika2232 <78856296+sonika2232@users.noreply.github.com>
2021-09-29 17:35:41 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,63 @@
+video="";
+status="";
+objects=[];
+
+function preload()
+{
+video= createVideo('video.mp4');
+video.size(480 , 280);
+video.hide();
+}
+
+function setup()
+{
+canvas= createCanvas(480 , 280);
+canvas.center();
+}
+
+function draw()
+{
+image(video , 0 , 0 , 480 , 280);
+if(status!="")
+{
+objectDetector.detect(video , gotResult);
+for(i=0; i < objects.length; i++)
+{
+document.getElementById("status").innerHTML="status: object detected";
+document.getElementById("number_of_objects").innerHTML="number of objects detected:" + objects.length;
+
+fill("#FF0000");
+percent = floor(objects[i].confidence * 100);
+text(objects[i].label + " " + percent + "%" , objects[i].x + 15, objects[i].y + 15);
+noFill();
+stroke("#FF0000");
+rect(objects[i].x , objects[i].y , objects[i].width , objects[i].height);
+}
+}
+}
+
+function gotResult(error , results)
+{
+if(error)
+{
+console.log(error);
+}
+console.log(results);
+objects= results;
+
+}
+
+function start()
+{
+objectDetector= ml5.objectDetector('cocossd' , modelLoaded);
+document.getElementById("status").innerHTML="status: detecting objects";
+}
+
+function modelLoaded()
+{
+console.log("modelLoaded");
+status= true;
+video.loop();
+video.speed(1);
+video.volume(0);
+}
